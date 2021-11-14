52a51684753d92d0cc59d5b2d26675e9fe623b2b
ART89-prog <dykpgilkom@mail.ru>
2021-11-14 18:32:15 +0300

    Первая версия готова
    Первая версия готова

--- smm-land-a/js/my.js
+++ smm-land-a/js/my.js
@@ -59,19 +59,22 @@ $(document).ready(function(){
 				breakpoint: 1024,
 				settings: {
 					slidesToShow: 2,
-			}
+                    slidesToScroll: 3,
+			    }
 			},
 			{
 			breakpoint: 768,
 			settings: {
 				slidesToShow: 1,
-			}
+                slidesToScroll: 1,
+			    }
 			},
 			{
 			breakpoint: 480,
 			settings: {
 				slidesToShow: 1,
-			}
+                slidesToScroll: 1,
+			    }
 			}
 		]
 	});
@@ -90,19 +93,22 @@ $(document).ready(function(){
                     breakpoint: 1024,
                     settings: {
                         slidesToShow: 2,
-                }
+                        slidesToScroll: 3,
+                    }
                 },
                 {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 1,
-                }
+                    slidesToScroll: 1,
+                    }
                 },
                 {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                }
+                    slidesToScroll: 1,
+                    }
                 }
             ]
         });
