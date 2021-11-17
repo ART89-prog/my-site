93c12c9f82d82e225f639c8bb90195754247b95e
ART89-prog <dykpgilkom@mail.ru>
2021-11-17 21:27:39 +0300

    Сделал страницу Вход в личный кабинет
    Сделал страницу Вход в личный кабинет

--- smm-lk-a/js/my.js
+++ smm-lk-a/js/my.js
@@ -38,6 +38,22 @@ $(document).ready(function(){
 
 
 
+    	// Пароль
+	$('body').on('click', '.password-control', function () {
+		if ($('#password-input').attr('type') == 'password') {
+			$(this).addClass('view');
+			$('#password-input').attr('type', 'text');
+		} else {
+			$(this).removeClass('view');
+			$('#password-input').attr('type', 'password');
+		}
+		return false;
+	});
+
+
+
+
+
     $(window).resize(() => {
         // Моб. версия
         if (!fiestResize) {
